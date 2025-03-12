d0f590f469ac07fe9a99081c4bf95de591a29ca4
Edwin Hollands <e.c.hollands@hotmail.co.uk>
2025-03-12 09:48:06 +0000

    got links too

--- Check_News.py
+++ Check_News.py
@@ -1,10 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
+
 url = 'https://novaramedia.com/'
-r=requests.get(url)
-r_html=r.text
+r = requests.get(url)
+r_html = r.text
 soup = BeautifulSoup(r_html, 'html.parser')
+
 print(f"{soup.title.string} Headlines:")
-titles = soup.find_all('h2')
-for title in titles:
-    print(f"{titles.index(title)+1}: {title.get_text()}")
+
+# Find all article titles and their corresponding links
+articles = soup.find_all('h2')
+for index, article in enumerate(articles):
+    title = article.get_text()
+    link = article.find('a')['href'] if article.find('a') else 'No link available'
+    print(f"{index + 1}: {title}\nLink: {link}\n")
